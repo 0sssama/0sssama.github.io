b0807e712c05ab2576f375ba78503551e3721779
Oussama Labrahmi <olabrahm@e2r9p15.1337.ma>
2022-01-13 16:13:48 +0100

    update to projects

--- projects.js
+++ projects.js
@@ -1,60 +1,68 @@
-const host = '.'
+const host = ".";
 const projects = [
-    {
-        id: 1,
-        title: 'MATCHING PROFILE PICTURES',
-        link: 'https://matchingpfps.com',
-        image: `${host}/imgs/matchingpfps.png`,
-        description_en: 'Matching profile pictures finder. 1000 average daily visitors.',
-        description_fr: 'Outil de recherche de photos de profils \'matching.\' Moyenne 1000 visiteurs chaque jour.',
-        langs: ['html', 'sass', 'js']
-    },
-    {
-        id: 2,
-        title: 'DAMMI APP',
-        link: 'https://dammiapp.com',
-        image: `${host}/imgs/dammiapp.jpeg`,
-        description_en: 'Website for Dammi App. Made in React.js, prototyped in Figma.',
-        description_fr: "Site Internet pour Dammi App. Realisé en React.js, prototype Figma.",
-        langs: ['html', 'sass', 'js']
-    },
-    {
-        id: 3,
-        title: 'PALAIS SHAZAM',
-        link: 'https://palaisshazam-mlq9x.ondigitalocean.app/',
-        image: `${host}/imgs/shazam.jpg`,
-        description_en: 'Website for a beautiful Moroccan Palace in the heart \
-                    of the Medina of Fes : Palais Shazam. Made in React.js, Node.js, Express.js & MongoDB.',
-        description_fr: "Site Internet pour un extrèmement beau Palais Marocain \
+  {
+    id: 1,
+    title: "MATCHING PROFILE PICTURES",
+    link: "https://matchingpfps.com",
+    image: `${host}/imgs/matchingpfps.png`,
+    description_en:
+      "Matching profile pictures finder. 1000 average daily visitors.",
+    description_fr:
+      "Outil de recherche de photos de profils 'matching.' Moyenne 1000 visiteurs chaque jour.",
+    langs: ["html", "sass", "js"],
+  },
+  {
+    id: 2,
+    title: "DAMMI APP",
+    link: "https://dammiapp.com",
+    image: `${host}/imgs/dammiapp.jpeg`,
+    description_en:
+      "Website for Dammi App. Made in React.js, prototyped in Figma.",
+    description_fr:
+      "Site Internet pour Dammi App. Realisé en React.js, prototype Figma.",
+    langs: ["html", "sass", "js"],
+  },
+  {
+    id: 3,
+    title: "PALAIS SHAZAM",
+    link: "https://palaisshazam-mlq9x.ondigitalocean.app/",
+    image: `${host}/imgs/shazam.jpg`,
+    description_en:
+      "Website for a beautiful Moroccan Palace in the heart \
+                    of the Medina of Fes : Palais Shazam. Made in React.js, Node.js, Express.js & MongoDB.",
+    description_fr:
+      "Site Internet pour un extrèmement beau Palais Marocain \
                         au coeur de la Medina du Fes : Palais Shazam. Realisé en React.js, Node.js, Express.js et MongoDB.",
-        langs: ['html', 'sass', 'js']
-    },
-    {
-        id: 4,
-        title: 'ICHRAQ BOUZIDI SHOP',
-        link: 'https://ichraqbouzidi.com',
-        image: `${host}/imgs/ichraq.png`,
-        description_en: 'Shopify theme for visual artist & story teller Ichraq Bouzidi.',
-        description_fr: "Un Theme Shopify pour l'artiste visuel et conteur Ichraq Bouzidi.",
-        langs: ['html', 'sass', 'js', 'shopify']
-    },
-    {
-        id: 5,
-        title: 'Vision Unik',
-        link: 'https://visionunikfes.com',
-        image: `${host}/imgs/visionunik.jpeg`,
-        description_en: 'Website for Vision Unik Fes. Made in React.js.',
-        description_fr: "Site Internet pour Vision Unik Fes. Realisé en React.js.",
-        langs: ['html', 'sass', 'js']
-    },
-    {
-        id: 6,
-        title: 'BACK-TRACKING SUDOKU SOLVING ALGORITHM',
-        link: 'https://labrahmi.me/sudoku-solver',
-        image: `${host}/imgs/sudoku.png`,
-        description_en: 'A Javascript back-tracking algorithm to solve any sudoku board in the world.',
-        description_fr: "Un algorithme Javascript du type back-tracking qui peut résourdre n'importe quel tableau \
-                        de sudoku au monde.",
-        langs: ['html', 'css' ,'js']
-    }
-]
+    langs: ["html", "sass", "js"],
+  },
+  {
+    id: 4,
+    title: "ICHRAQ BOUZIDI SHOP",
+    link: "https://ichraqbouzidi.com",
+    image: `${host}/imgs/ichraq.png`,
+    description_en:
+      "Shopify theme for visual artist & story teller Ichraq Bouzidi.",
+    description_fr:
+      "Un Theme Shopify pour l'artiste visuel et conteur Ichraq Bouzidi.",
+    langs: ["html", "sass", "js", "shopify"],
+  },
+  {
+    id: 5,
+    title: "Vision Unik",
+    link: "https://visionunikfes.com",
+    image: `${host}/imgs/visionunik.jpeg`,
+    description_en: "Website for Vision Unik Fes. Made in React.js.",
+    description_fr: "Site Internet pour Vision Unik Fes. Realisé en React.js.",
+    langs: ["html", "sass", "js"],
+  },
+  {
+    id: 6,
+    title: "MediaEthic",
+    link: "https://media-ethic.vercel.app",
+    image: `${host}/imgs/mediaethic.jpeg`,
+    description_en: "Website for MediaEthic agency. Made in Next.js.",
+    description_fr:
+      "Site Internet pour l'agence MediaEthic. Realisé en Next.js.",
+    langs: ["html", "css", "js"],
+  },
+];
